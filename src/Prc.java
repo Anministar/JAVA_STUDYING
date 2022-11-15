7cee6848aba378a816b0919c22d72f16a3f6fd81
popo <popo@poposmacbookpro>
2022-11-15 21:30:12 +0900

    STUDYING_2022_11_15 TUES

--- src/Prc.java
+++ src/Prc.java
@@ -6,22 +6,21 @@ public class Prc {
 
 	public static void main(String[] args) {
 		
-		Map <String, Integer> map = new HashMap();
-		
-		map.put("aaa", 1111);
-		map.put("bbb", 2222);
-		map.put("ccc", 3333);
-		map.put("ddd", 4444);
-		
-		
-		Collection<Integer> collection = map.values();
-		for (Integer value : collection) {
-			System.out.println(value);
-		}
-		
-		
+//		Map <String, Integer> map = new HashMap();
+//		
+//		map.put("aaa", 1111);
+//		map.put("bbb", 2222);
+//		map.put("ccc", 3333);
+//		map.put("ddd", 4444);
+//		
+//		
+//		Collection<Integer> collection = map.values();
+//		for (Integer value : collection) {
+//			System.out.println(value);
+//		}
 		
 		
+			System.out.println("dlehdgksms cjswodlek");
 		
 		
 			
